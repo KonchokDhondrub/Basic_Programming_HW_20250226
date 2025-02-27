0a83cc4e26de409d3f9e5a0143f5f50b85f4e326
kd <erm.melnik@gmail.com>
2025-02-27 11:35:46 +0100

    Reformating code. Adding different type of method.

--- src/Main2.java
+++ src/Main2.java
@@ -10,18 +10,44 @@ public class Main2 {
         List<String> result = getMoreThan5SimilarStrings(list);
         System.out.println(result);
     }
-
     public static List<String> getMoreThan5SimilarStrings(List<String> list) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : list) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= 5) {
+                result.add(entry.getKey());
+            }
+        }
+        return result;
+    }
+
+    public static List<String> getMoreThan5SimilarStrings_Collections(List<String> list) {
+        List<String> result = new ArrayList<>();
+        for (String str : new HashSet<>(list)) {
+            int count = Collections.frequency(list, str);
+            if (count >= 5) {
+                result.add(str);
+            }
+        }
+        return result;
+    }
+
+    public static List<String> getMoreThan5SimilarStrings_On2(List<String> list) {
         List<String> result = new ArrayList<>();
 
-        for (String str : list) {
+        for (String str : new HashSet<>(list)) {
             int count = 0;
             for (String s : list) {
                 if (s.equals(str)) {
                     count++;
+                    if (count >= 5) break;
                 }
             }
-            if (count >= 5 && !result.contains(str)) {
+            if (count >= 5) {
                 result.add(str);
             }
         }
