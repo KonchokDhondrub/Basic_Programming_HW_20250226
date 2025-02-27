4495ceb278b6e91840de444e245c21cdd49d8bcb
kd <erm.melnik@gmail.com>
2025-02-27 10:46:33 +0100

    Reformating code.

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
     }
 
     public static void mergeKeysAndValues(Map<String, Integer> map, String key1, String key2) {
-        if (map.containsKey(key1) && map.containsKey(key2)) {
+        if (map!=null && map.containsKey(key1) && map.containsKey(key2)) {
             String newKey = key1 + "_" + key2;
             int sum = map.get(key1) + map.get(key2);
             map.put(newKey, sum);
